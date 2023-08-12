fce99b57ae935f42dbc12526a15d88ee9f56895c
emanuel santoss <emanuel.jesus3@etec.sp.gov.br>
2023-08-12 17:17:43 -0300

    personalização completa, com os dois temas, dark e light prontas

--- javascript.js
+++ javascript.js
@@ -1,15 +1,35 @@
-let darkThema = true
-const pegandoSwitch = document.getElementById('switch');
-const pegandoTack = document.getElementById('raiz');
+// O MEU JEITO DE FAZER!!!
+/* let pegandoThemaModel = document.querySelector('html').classList.value
+// console.log(pegandoThemaModel);
 
+// function toggleMode() {
+//   if (pegandoThemaModel === 'light') {
+//     document.querySelector('html').classList.remove('light');
+//     pegandoThemaModel = ''
+//   } else {
+//     document.querySelector('html').classList.add('light');
+//     pegandoThemaModel = 'light'
+//   }
+} */
 
-pegandoSwitch.addEventListener('click', function () {
-  console.log('Clicou aquiii');
-});
+// O JEITO DO DISCOVER
 
-pegandoSwitch.addEventListener("click", function () {
-  console.log("clicou aqui");
-  if (!darkThema) {
-    pegandoTack.classList.add("light");
+function toggleMode() {
+  const html = document.documentElement
+  const imgProfile = document.getElementById('profile').querySelector('img');
+  html.classList.toggle('light');
+
+  if (html.classList.contains('light')) {
+    imgProfile.setAttribute('src', './assets/avatar-light.png');
+  } else {
+    imgProfile.setAttribute('src', './assets/avatar.png');
   }
-});
+
+  /* Jeito Dificil sem conhecer o javascript e seus atributos
+// if (html.classList.contains('light')) {
+//   html.classList.remove('light');
+// } else {
+//   html.classList.add('light');
+// }
+*/
+}
